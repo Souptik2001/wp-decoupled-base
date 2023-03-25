49cef6543cc541897330136a999c794bd37fa40d
Souptik Datta <souptikdatta2001@gmail.com>
2023-03-25 13:35:01 +0530

    feat: Add routing for blog archive and blog single page
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog';
 import BlogList from './components/BlogList';
 import Navbar from './components/Navbar';
 import PageNotFound from './components/PageNotFound';
@@ -12,6 +13,8 @@ function App() {
         <div className="container">
           <Routes>
             <Route path='/' element={<BlogList />} />
+            <Route path='/:taxonomy/:slug' element={<BlogList />} />
+            <Route path='/blog/:slug' element={<Blog />} />
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
