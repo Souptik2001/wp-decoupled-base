4cfd114eee493516d35658b9a98968a103deae88
Souptik Datta <souptikdatta2001@gmail.com>
2023-03-27 14:28:48 +0530

    feat: Add pagination links
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- test-app/src/components/BlogList.jsx
+++ test-app/src/components/BlogList.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from "@apollo/client";
 import { useContext, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import '../BlogList.css';
+import '../Navigation.css';
 import { GET_POSTS } from "../queries/Posts";
 import BlogListCard from "./BlogList/BlogListCard";
 import { PostStoreContext } from "./BlogListRoutes";
 
 function BlogList() {
-	const {postType="", taxonomy="", term="", page=0} = useParams();
+	const {postType="", taxonomy="", term="", page=1} = useParams();
 	const pageNumber = parseInt(page);
 
 	const {posts, setPosts} = useContext(PostStoreContext);
@@ -17,7 +19,7 @@ function BlogList() {
 			variables: {
 				limit: 10,
 				postType: postType,
-				offset: pageNumber*10,
+				offset: Math.max(pageNumber-1, 0)*10,
 			}
 		}
 	);
@@ -42,6 +44,29 @@ function BlogList() {
 			{
 				! loading
 				&&
+				data?.posts?.pageInfo
+				&&
+				(
+					<div className="navigation">
+						<Link to={`${navigationLink}/${Math.max(pageNumber-1, 1) <= 1 ? '' : Math.max(pageNumber-1, 1)}`}>Prev</Link>
+						{
+							pageNumber-1 > 0
+							&&
+							<Link to={`${navigationLink}/${pageNumber-1}`}>{pageNumber-1}</Link>
+						}
+						<Link className="is-active" to={`${navigationLink}/${pageNumber}`}>{pageNumber}</Link>
+						{
+							pageNumber+1 <= data?.posts?.pageInfo?.totalPages+1
+							&&
+							<Link to={`${navigationLink}/${pageNumber+1}`}>{pageNumber+1}</Link>
+						}
+						<Link to={`${navigationLink}/${Math.min(pageNumber+1, data?.posts?.pageInfo?.totalPages+1)}`}>Next</Link>
+					</div>
+				)
+			}
+			{
+				! loading
+				&&
 				data?.posts
 				&&
 				data?.posts?.posts.length > 0
@@ -51,18 +76,6 @@ function BlogList() {
 						<BlogListCard key={val.slug} blogData={val} postType={postType} />
 					);
 				})
-			}
-			{
-				! loading
-				&&
-				data?.posts?.pageInfo
-				&&
-				(
-					<div>
-						<Link to={`${navigationLink}/${Math.max(pageNumber-1, 0) === 0 ? '' : Math.max(pageNumber-1, 0)}`}>Prev</Link>
-						<Link to={`${navigationLink}/${Math.min(pageNumber+1, data?.posts?.pageInfo?.totalPages)}`}>Next</Link>
-					</div>
-				)
 			}
 		</div>
 	)
