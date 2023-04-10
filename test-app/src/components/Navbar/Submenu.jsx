9a40c8327de79df4c8414f01184086c3c0028f81
Souptik Datta <souptikdatta2001@gmail.com>
2023-04-11 01:09:19 +0530

    fix: Fix nav-menu hover
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- test-app/src/components/Navbar/Submenu.jsx
+++ test-app/src/components/Navbar/Submenu.jsx
@@ -4,24 +4,26 @@ import { Link } from 'react-router-dom';
 const Submenu = forwardRef(({submenu}, ref) => {
 
 	return (
-		<ul className="submenu" ref={ref}>
-			{
-				submenu &&
-				submenu.map((val) => {
-					return (
-						<li key={val['menu_order']} className={(val.child_items && val.child_items.length > 0) ? "has-child-menu menu-item" : "menu-item"}>
-							<Link to={val.url}>{val.title}</Link>
-							{
-								val.child_items
-								&&
-								val.child_items.length > 0
-								&&
-								<Submenu submenu={val.child_items} />
-							}
-						</li>
-					)
-				})
-			}
+		<ul className="submenu-wrapper" ref={ref}>
+			<div className="submenu">
+				{
+					submenu &&
+					submenu.map((val) => {
+						return (
+							<li key={val['menu_order']} className={(val.child_items && val.child_items.length > 0) ? "has-child-menu menu-item" : "menu-item"}>
+								<Link to={val.url}>{val.title}</Link>
+								{
+									val.child_items
+									&&
+									val.child_items.length > 0
+									&&
+									<Submenu submenu={val.child_items} />
+								}
+							</li>
+						)
+					})
+				}
+			</div>
 		</ul>
 	)
 
