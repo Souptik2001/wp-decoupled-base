4f7930959033de454a3d1880e1d8e8ec439383df
Souptik Datta <souptikdatta2001@gmail.com>
2023-04-19 01:25:37 +0530

    feat: Implement docker based setup
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

---
+++ backend/docker-build-utils/setup-wp.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+cd /var/www/html
+
+pwd
+
+wp core install --url=http://localhost:8080 --title=Test --admin_user=admin --admin_password=admin --admin_email=admin@test.com --allow-root
+
+apt install mariadb-client -y
+
+wp db import ./wp-content/wordpress-2023-04-18-55b70e7.sql --allow-root
+
+wp search-replace "https://test.lndo.site" "http://localhost:8080" --all-tables --allow-root
+
+wp search-replace "test.lndo.site" "localhost:8080" --all-tables --allow-root
+
+wp plugin activate wp-rest-api-v2-menus --allow-root
+
--- test-app/src/mutations/PostMeta.jsx
+++ test-app/src/mutations/PostMeta.jsx
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const UPDATE_POST_META = gql`
-	mutation UpdatePostMeta($postType: String, $postID: Int, $metaInput: [MetaInput!]!) {
+	mutation UpdatePostMeta($postType: String!, $postID: Int!, $metaInput: [MetaInput!]!) {
 		updatePostMeta(postType: $postType, postID: $postID, metaInput: $metaInput) {
 			success
 		}
